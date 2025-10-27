39ac4290001aab0cb4658439b88942d0939132a2
Anwar <98222297+Heechanwan@users.noreply.github.com>
2025-10-27 12:56:49 +0500

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Replace 'repo-name' with your actual GitHub repository name
 export default defineConfig({
   plugins: [react()],
-  base: '/testCGS/', // Use your repository name, e.g., '/my-app/' (include trailing slash)
+  base: '/testCGS/' // Правильно для репозитория testCGS
 });
